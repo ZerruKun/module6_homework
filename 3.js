9f162bc45463744d4f2a4d715f3ba8689c9a4308
Tabalin Nikolay <Zerru_Kun@yahoo.com>
2022-09-30 14:11:27 +0300

    Ex 3.

---
+++ 3.js
@@ -0,0 +1,17 @@
+const getSum = (num1) => {
+    return (num2) => {
+        reuslt = num1 + num2;
+        if(typeof(reuslt) !== "number" || isNaN(reuslt)) {
+            return "Одно из значений не является числом."
+        } else {
+            return reuslt;
+        }
+    }
+}
+
+let number1 = 10;
+let number2 = 5;
+let number3 = "Not a number - just a string";
+let number4 = 15;
+console.log(getSum(number1)(number2)); // 15
+console.log(getSum(number3)(number4)); // Одно из значений не является числом.
